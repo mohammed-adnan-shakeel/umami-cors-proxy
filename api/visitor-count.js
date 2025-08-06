b777ec926b1a0dc3d48da61cdc5abfc65fe121eb
MOHAMMED ADNAN SHAKEEL <adnanshakeel1911@gmail.com>
2025-08-07 02:40:10 +0530

    Update and rename share.js to visitor-count.js

---
+++ api/visitor-count.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const websiteId = "8b3ce781-d545-4fa6-84d0-17201616071a";
+  const apiToken = process.env.UMAMI_API_TOKEN;
+  const startAt = 1622505600000;
+  const endAt = Date.now();
+
+  try {
+    const response = await fetch(
+      `https://umami-mas.vercel.app/api/websites/${websiteId}/stats?startAt=${startAt}&endAt=${endAt}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "Failed to fetch stats" });
+    }
+
+    const data = await response.json();
+    return res.status(200).json({ visitors: data.pageviews.value });
+  } catch (err) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
